5699df8dd62f582fd78597a41837be6856c01d3b
Pablo Stafforini <pablo@stafforini.com>
2023-05-09 09:36:39 -0300

    Remove key bindings: they are now defined in config.org

--- tlon-bae.el
+++ tlon-bae.el
@@ -1139,11 +1139,5 @@ If DIR-PATH is nil, create a command to open the BAE repository."
    ]
   )
 
-;;; key bindings
-(global-set-key (kbd "H-r") 'tlon-bae-dispatch)
-(define-key github-review-mode-map (kbd "s-c") 'tlon-bae-submit-comment-revisions)
-(define-key markdown-mode-map (kbd "s-f") 'tlon-bae-finalize-revision)
-
-
 (provide 'tlon-bae)
 ;;; tlon-bae.el ends here
