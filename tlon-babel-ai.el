6a4e70a0c914ffa9aab205890e6399d511308cba
Pablo Stafforini <pablo@stafforini.com>
2024-03-19 07:57:25 +0530

    ai: capitalize section names

--- tlon-babel-ai.el
+++ tlon-babel-ai.el
@@ -55,7 +55,7 @@
   "`gptel' failed with message: %s"
   "Error message to display when `gptel-quick' fails.")
 
-;;;;; language detection
+;;;;; Language detection
 
 (defconst tlon-babel-ai-detect-language-common-prompts
   (format ":%s. Your answer should just be the language of the entry. For example, if you conclude that the language is English, your answer should be just 'english'. Moreover, your answer can be only one of the following languages: %s" tlon-babel-ai-string-wrapper
@@ -72,7 +72,7 @@
 	  tlon-babel-ai-detect-language-common-prompts)
   "Prompt for language detection.")
 
-;;;;; translation
+;;;;; Translation
 
 (defconst tlon-babel-ai-translate-prompt
   (format "Translate the following text into Spanish:%s" tlon-babel-ai-string-wrapper)
@@ -83,13 +83,13 @@
   (format "Please generate the best ten Spanish translations of the following English text:%s. Please return each translation on the same line, separated by '|'. Do not add a space either before or after the '|'. Do not precede your answer by 'Here are ten Spanish translations' or any comments of that sort: just return the translations. An example return string for the word 'very beautiful' would be: 'muy bello|muy bonito|muy hermoso|muy atractivo' (etc). Thanks!" tlon-babel-ai-string-wrapper)
   "Prompt for translation variants.")
 
-;;;;; rewriting
+;;;;; Rewriting
 
 (defconst tlon-babel-ai-rewrite-prompt
   (format "Por favor, genera las mejores diez variantes del siguiente texto castellano:%s. Por favor, devuelve todas las variantes en una única linea, separadas por '|'. No insertes un espacio ni antes ni después de '|'. No agregues ningún comentario aclaratorio: solo necesito la lista de variantes. A modo de ejemplo, para la expresión 'búsqueda de poder' el texto a devolver sería: 'ansia de poder|ambición de poder|búsqueda de autoridad|sed de poder|afán de poder|aspiración de poder|anhelo de poder|deseo de control|búsqueda de dominio|búsqueda de control' (esta lista solo pretende ilustrar el formato en que debes presentar tu respuesta). Gracias!" tlon-babel-ai-string-wrapper)
   "Prompt for rewriting.")
 
-;;;;; summarization
+;;;;; Summarization
 
 (defconst tlon-babel-ai-how-to-write-summary-prompt
   `((:prompt "Write the abstract in a sober, objective tone, avoiding cliches, excessive praise and unnecessary flourishes. In other words, draft it as if you were writing the abstract of a scientific paper. The abstract should be only one paragraph long and have a rough length of 100 to 250 words (feel free to exceed it if you really need to, but never go over 350 words). It should not mention bibliographic data of the work (such as title or author). Write the abstract directly stating what the article argues, rather than using phrases such as 'The article argues that...'. For example, instead of writing 'The article ‘The eradication of smallpox’ by William D. Tierney tells that mankind fought smallpox for centuries...', write 'Mankind fought smallpox for centuries...'. Also, please omit any disclaimers of the form 'As an AI language model, I'm unable to browse the internet in real-time.' Finally, end your abstract with the phrase ' – AI-generated abstract.'"
