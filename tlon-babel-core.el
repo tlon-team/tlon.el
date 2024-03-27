83a167580d711b747e9081e8ac17f4507235281d
Pablo Stafforini <pablo@stafforini.com>
2024-03-27 08:58:07 -0300

    babel: add missing repos to tlon-babel-repos

--- tlon-babel-core.el
+++ tlon-babel-core.el
@@ -119,14 +119,6 @@
 	   :language "es"
 	   :key "q s"
 	   :url "https://altruismoeficaz.net/")
-    (:name "uqbar-it"
-	   :project "babel"
-	   :subproject "uqbar"
-	   :abbrev "uqbar-it"
-	   :type content
-	   :subtype translations
-	   :language "it"
-	   :key "q t")
     (:name "uqbar-fr"
 	   :project "babel"
 	   :subproject "uqbar"
@@ -135,6 +127,16 @@
 	   :subtype translations
 	   :language "fr"
 	   :key "q f")
+	   :url "")
+    (:name "uqbar-it"
+	   :project "babel"
+	   :subproject "uqbar"
+	   :abbrev "uqbar-it"
+	   :type content
+	   :subtype translations
+	   :language "it"
+	   :key "q t"
+	   :url "")
     (:name "utilitarianism-en"
 	   :project "babel"
 	   :subproject "utilitarianism"
