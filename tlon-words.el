03e1b621ed1a3b8d5923fbb777599275d5cdc602
Pablo Stafforini <pablo@stafforini.com>
2024-05-28 10:46:32 -0300

    words: develop org, gdrive export functionality

--- tlon-words.el
+++ tlon-words.el
@@ -30,53 +30,152 @@
 (require 'tlon-md)
 (require 'tlon-yaml)
 
+;;;; Variables
+
+(defconst tlon-words-gdrive-directory-ids
+  '(("de" . "1K9OuZSP1yB-2m7G5pib9DkNqyE17bwJ-")
+    ("en" . "13WXtjz18x3jSz5NI080SfTJAdWMN-zvC")
+    ("es" . "1uwu3CyVdOa3o6NgohQTbMwqQ451fKP8p")
+    ("fr" . "12HCj4y9qkA-Mlr4O_pv1AunPzJr76ata")
+    ("it" . "1NW7DZIqBqsZIlWAgyxM5VuSJzirwPvOs")
+    ("pt" . "1rsSiyWLFY9HbcIeyML0mxM7FiWSLw7u0"))
+  "Alist of Google Drive directories and associated IDs.")
+
 ;;;; Functions
 
 ;;;;; Commands
 
 ;;;###autoload
-(defun tlon-count-words-in-repo (&optional repo-name regexp)
+(defun tlon-count-words-in-repo (&optional repo-name format regexp)
   "Return the number of words in REPO-NAME whose file names match REGEXP.
-If REPO is nil, prompt the user for one. If REGEXP is nil, restrict return the
-number of words in Markdown files."
+If REPO is nil, prompt the user for one.
+
+FORMAT is the format in which to return the results. If `list', return a list of
+cons cells. If `count', return the word count as a number. If `summary', or any
+other value, return a message with the word count and the file count.
+
+If REGEXP is nil, restrict return the number of words in Markdown files."
   (interactive)
   (let* ((repo-name (or repo-name
 			(completing-read "Repo: "
 					 (tlon-repo-lookup-all :abbrev :subtype 'translations))))
 	 (dir (tlon-repo-lookup :dir :name repo-name)))
-    (tlon-count-words-in-dir dir regexp)))
+    (tlon-count-words-in-dir dir format regexp 'recursively)))
 
 ;;;###autoload
-(defun tlon-count-words-in-dir (&optional dir regexp)
+(defun tlon-count-words-in-dir (&optional dir format regexp recursively)
   "Return the number of words files in DIR whose file names match REGEXP.
-If DIR is nil, use the directory at point, else prompt the user for one. If
-REGEXP is nil, restrict return the number of words in Markdown files."
+If DIR is nil, use the directory at point, else prompt the user for one.
+
+FORMAT is the format in which to return the results. If `list', return a list of
+cons cells. If `count', return the word count as a number. If `summary', or any
+other value, return a message with the word count and the file count.
+
+If REGEXP is nil, restrict return the number of words in Markdown files."
   (interactive)
   (if-let ((dir (or dir
 		    (let ((file-at-point (thing-at-point 'filename)))
 		      (when (file-directory-p file-at-point)
 			file-at-point)))))
       (let* ((regexp (or regexp "\\.md$"))
-	     (files (directory-files-recursively dir regexp)))
-	(tlon-count-words-in-files files))
+	     (files (if recursively
+			(directory-files-recursively dir regexp)
+		      (directory-files dir t regexp))))
+	(tlon-count-words-in-files files format))
     (user-error "No directory found")))
 
 ;;;###autoload
-(defun tlon-count-words-in-files (&optional files)
+(defun tlon-count-words-in-files (&optional files format)
   "Return the number of words in the list of FILES.
-If FILES is nil, use the selected files in Dired, if possible."
+If FILES is nil, use the selected files in Dired, if possible.
+
+FORMAT is the format in which to return the results. If `list', return a list of
+cons cells. If `count', return the word count as a number. If `summary', or any
+other value, return a message with the word count and the file count."
   (interactive)
   (when-let ((files (or files (dired-get-marked-files))))
     (let ((initial-buffers (buffer-list))
-	  (in-total 0))
-      (dolist (file files in-total)
+	  list)
+      (dolist (file files list)
 	(with-temp-buffer
 	  (insert-file-contents file)
-	  (let ((in-file (tlon-count-substantive-words)))
-	    (setq in-total (+ in-total in-file)))
-	  (unless (member (current-buffer) initial-buffers)
-	    (kill-buffer (current-buffer)))))
-      (message (number-to-string in-total)))))
+	  (push (cons (tlon-count-substantive-words) (file-name-nondirectory file)) list))
+	(unless (member (current-buffer) initial-buffers)
+	  (kill-buffer (current-buffer))))
+      (let ((word-count (apply '+ (mapcar 'car list)))
+	    (file-count (length list)))
+	(pcase format
+	  ('list (reverse list))
+	  ('count word-count)
+	  (_ (message "%s words in %s files." word-count file-count)))))))
+
+;;;;; Org export
+
+(declare-function dired-filename-at-point "dired-x")
+;;;###autoload
+(defun tlon-words-create-table-for-dir (&optional dir)
+  "Create an `org-mode' table with the word counts for all Markdown files in DIR.
+If DIR is nil, use the directory at point, else prompt the user for one."
+  (interactive)
+  (let* ((dir (or dir (dired-filename-at-point)))
+	 (list (tlon-count-words-in-dir dir 'list))
+	 (destination (tlon-words-get-table-output-path dir)))
+    (tlon-words-insert-table-from-list list destination)
+    (tlon-words-export-org-table destination)
+    (tlon-words-export-to-google-drive (tlon-words-get-tsv-file destination))))
+
+(declare-function org-table-align "org-table")
+(defun tlon-words-insert-table-from-list (list &optional destination)
+  "Insert an `org-mode' table into a new buffer from LIST.
+LIST is a list of cons cells. If DESTINATION is non-nil, save the table to it."
+  (let ((buffer (generate-new-buffer "*Tlön stats*")))
+    (switch-to-buffer buffer)
+    (dolist (pair list)
+      (insert (format "| %s | %s |\n" (cdr pair) (car pair))))
+    (org-mode)
+    ;; (org-table-align) ; extremely slow!
+    (goto-char (point-min))
+    (when destination
+      (write-file destination))))
+
+(defun tlon-words-get-table-output-path (dir)
+  "Return the path of the `org-mode' table file for DIR."
+  (let* ((target-dir (file-name-concat (tlon-get-repo 'error) "stats/"))
+	 (filename-base (file-name-nondirectory (directory-file-name dir)))
+	 (filename (file-name-with-extension filename-base "org")))
+    (file-name-concat target-dir filename)))
+
+;;;;; Google Drive export
+
+(declare-function org-table-export "org-table")
+(defun tlon-words-export-org-table (&optional file)
+  "Export the `org-mode' table in FILE to a `tsv' file.
+If FILE is nil, use the file visited by the current buffer."
+  (interactive)
+  (let* ((file (or file (buffer-file-name)))
+	 (destination (tlon-words-get-tsv-file file)))
+    (with-current-buffer (find-file-noselect file)
+      (org-table-export destination "orgtbl-to-tsv"))))
+
+(defun tlon-words-get-tsv-file (file)
+  ""
+  (let* ((target-dir (file-name-directory file))
+	 (target-file-base (file-name-base (file-name-nondirectory file)))
+	 (target-file (file-name-with-extension target-file-base "tsv")))
+    (file-name-concat target-dir target-file)))
+
+(defun tlon-words-export-to-google-drive (&optional file)
+  "Export FILE to Google Drive.
+If FILE is nil, export the file visited by the current buffer."
+  (interactive)
+  (let* ((dir-id (alist-get (tlon-repo-lookup :language :dir (tlon-get-repo 'error))
+			    tlon-words-gdrive-directory-ids nil nil 'string=))
+	 (output (shell-command (format "gdrive files import %s --parent %s" file dir-id)))
+	 (url (format "https://drive.google.com/drive/folders/%s" dir-id)))
+    (pcase output
+      (0 (message "File exported to Google Drive."))
+      (_ (user-error "Google Drive export error")))
+    (browse-url url)))
 
 ;;;;; Filter count
 
@@ -100,7 +199,6 @@ defined in `tlon-count-extraneous-words'."
     (widen)
     (let ((raw (count-words (point-min) (point-max))))
       (- raw (tlon-count-extraneous-words)))))
-
 
 ;;;;; Misc
 
