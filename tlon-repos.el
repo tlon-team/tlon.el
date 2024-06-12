b826e6ae56bf9da949886e5162f1df954e6a1f17
Pablo Stafforini <pablo@stafforini.com>
2024-06-12 12:43:38 -0300

    repos: add forge search commands and menu

--- tlon-repos.el
+++ tlon-repos.el
@@ -134,6 +134,143 @@ If DIR is nil, use the current directory."
     (dolist (repo repos)
       (tlon-pull-issues-in-repo repo))))
 
+;;;;; Search
+
+(defun tlon-forge-search-titles (string)
+  "Search for STRING in the title of the current repo."
+  (interactive "sSearch string: ")
+  (tlon-forge-search string (list default-directory) nil))
+
+(defun tlon-forge-search-titles-in-all-repos (string)
+  "Search for STRING in the title of issues in all Tlön repos."
+  (interactive "sSearch string: ")
+  (tlon-forge-search string))
+
+(defun tlon-forge-search-in-all-repos (string)
+  "Search for STRING in the title and body of issues in all Tlön repos."
+  (interactive "sSearch string: ")
+  (tlon-forge-search string nil 'full))
+
+(defun tlon-forge-search (string &optional repos full)
+  "Perform a search for STRING in the title of issues and pull requests of REPOS.
+If REPOS is nil, use a list of full the Tlön repos. By default, search in titles
+only. If FULL is non-nil, search also in the body of issues and pull requests."
+  (let* ((repos (or repos (tlon-repo-lookup-all :dir)))
+	 (matches))
+    (dolist (repo repos)
+      (when-let* ((default-directory repo)
+		  (db (forge-db))
+		  (repo (forge-get-repository ':tracked?))
+		  (repoid (slot-value repo 'id)))
+	(emacsql-with-transaction db
+	  (emacsql db [:create-virtual-table :if :not :exists search
+					     :using :fts5
+					     ([id haystack author date type title body])])
+	  (emacsql db
+		   (concat "insert into search "
+			   "select id, "
+			   (if full
+			       "('\"' || replace(title, '\"', ' ') || ' ' || replace(body, '\"', ' ') || '\"') as haystack, "
+			     "('\"' || replace(title, '\"', ' ') || '\"') as haystack, ")
+			   "author, " forge-search-date-type " as date, "
+			   "'\"issue\"' as type, title, body "
+			   "from issue where repository = '\"" repoid "\"';"))
+	  (when full
+	    (emacsql db
+		     (concat "insert into search "
+			     "select issue_post.issue, "
+			     "('\"' || replace(issue_post.body, '\"', ' ') || '\"') as haystack, "
+			     "issue_post.author, "
+			     "issue_post." forge-search-date-type " as date, "
+			     "'\"issue_msg\"' as type, '\"\"' as title, issue_post.body "
+			     "from issue_post "
+			     "inner join issue on issue.id = issue_post.issue "
+			     "where issue.repository = '\"" repoid "\"';")))
+	  (emacsql db
+		   (concat "insert into search "
+			   "select id, "
+			   (if full
+			       "('\"' || replace(title, '\"', ' ') || ' ' || replace(body, '\"', ' ') || '\"') as haystack, "
+			     "('\"' || replace(title, '\"', ' ') || '\"') as haystack, ")
+			   "author, " forge-search-date-type " as date, "
+			   "'\"pr\"' as type, title, body "
+			   "from pullreq where repository = '\"" repoid "\"';"))
+	  (when full
+	    (concat "insert into search "
+		    "select pullreq_post.pullreq, "
+		    "('\"' || replace(pullreq_post.body, '\"', ' ') || '\"') as haystack, "
+		    "pullreq_post.author, "
+		    "pullreq_post." forge-search-date-type " as date, "
+		    "'\"pr_msg\"' as type, '\"\"' as title, pullreq_post.body "
+		    "from pullreq_post "
+		    "inner join pullreq on pullreq.id = pullreq_post.pullreq "
+		    "where repository = '\"" repoid "\"';"))
+	  (let* ((magit-generate-buffer-name-function (lambda (_mode _value)
+							(format "*Forge Search Results: %s*" (slot-value repo 'name)))))
+	    (setq matches (emacsql db [:select [id author date type title body]
+					       :from search
+					       :where haystack :match $r1]
+				   string))
+	    (when matches
+	      (magit-setup-buffer #'forge-search-mode t
+		(matches matches)
+		(search-string string))))
+	  (emacsql db [:drop-table search]))))))
+
+(transient-define-prefix tlon-forge-menu ()
+"Dispatch a forge command."
+[:if forge--get-repository:tracked?
+     ["Create"
+      ("c i" "issue"             forge-create-issue)
+      ("c p" "pull-request"      forge-create-pullreq)
+      ("c u" "pull-request from issue"
+       forge-create-pullreq-from-issue
+       :if forge--get-github-repository)
+      ("c f" "fork or remote"    forge-fork)]
+     ["Search"
+      "full"
+      ("s s" "this repo"      forge-search)
+      ("s a" "all repos"      tlon-forge-search-in-all-repos)
+      "titles only"
+      ("s S" "this repo"      tlon-forge-search-titles)
+      ("s A" "all repos"      tlon-forge-search-titles-in-all-repos)]]
+[:if forge--get-repository:tracked?
+     ["List"
+      ("t" "topics...         "  forge-topics-menu        :transient replace)
+      ("n" "notifications...  "  forge-notifications-menu :transient replace)
+      ("r" "repositories...   "  forge-repositories-menu  :transient replace)]
+     ["Fetch"
+      ("f f" "all topics       " forge-pull)
+      ("f t" "one topic        " forge-pull-topic)
+      ("f n" "notifications    " forge-pull-notifications)]
+     ["Do"
+      :if forge--get-repository:tracked?
+      ("C" "configure"       forge-configure)
+      ("M" "merge w/api"     forge-merge :level 7)]]
+[:if forge--get-repository:tracked?
+     ["Visit"
+      ("v t" "topic"         forge-visit-topic)
+      ("v i" "issue"         forge-visit-issue)
+      ("v p" "pull-request"  forge-visit-pullreq)]
+     ["Browse"
+      ("b t" "topic"         forge-browse-topic)
+      ("b i" "issue"         forge-browse-issue)
+      ("b p" "pull-request"  forge-browse-pullreq)]
+     ["Browse"
+      ("b r" "remote"        forge-browse-remote)
+      ("b I" "issues"        forge-browse-issues)
+      ("b P" "pull-requests" forge-browse-pullreqs)]]
+[[:description (lambda ()
+                 (if (magit-gitdir)
+                     "Forge doesn't know about this Git repository yet"
+                   "Not inside a Git repository"))
+	       :if-not forge--get-repository:tracked?
+	       ("a" "add repository to database" forge-add-repository)
+	       ("f" "fetch notifications"        forge-pull-notifications)
+	       ("l" "list notifications"         forge-list-notifications)]])
+
+(advice-add 'forge-dispatch :override #'tlon-forge-menu)
+
 ;;;; Menu
 
 ;;;###autoload (autoload 'tlon-repos-menu "tlon-repos" nil t)
