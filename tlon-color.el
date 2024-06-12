4d0e181f9fa7eaae4ee7a54289aadee1f956bf09
Pablo Stafforini <pablo@stafforini.com>
2024-06-12 14:35:07 -0300

    color: thoroughly refactor feature

--- tlon-color.el
+++ tlon-color.el
@@ -36,39 +36,70 @@
   "`tlon-color' group."
   :group 'tlon)
 
-(defcustom tlon-color-css-format "HEX"
-  "Format for the color values in the css file."
-  :type '(choice (const "HEX")
-		 (const "HSL"))
-  :group 'tlon-color)
+(defcustom tlon-color-format-in-frontend-files
+  "hex"
+  "Color format to use in the frontend files when setting a color palette.
+Allowed values are \"hex\" and \"hsl\"."
+  :group 'tlon-color
+  :type '(choice (const :tag "Hex" "hex")
+		 (const :tag "HSL" "hsl")))
+
+(defcustom tlon-colors-change-step
+  1
+  "Step to use when changing the hue, saturation or lightness of a color."
+  :group 'tlon-color
+  :type 'integer)
 
 ;;;; Variables
-
-;;;;; Files
-
-(defvar tlon-color-palette-file "tlon-color-palettes.el"
-  "File where custom color palettes are persisted.")
-
-(defvar tlon-color-globals-file
-  (file-name-concat (tlon-repo-lookup :dir :name "uqbar-front")
-		    "src/styles/globals.css")
-  "Path to the `globals.css' file.")
-
-;;;;; Color variables
 
 (defconst tlon-color-palettes '()
   "List of color palettes and their light and dark values.")
 
-;;;;; CSS elements
+;;;;; Files
 
-(defconst tlon-color-css-header
+;; TODO: do not hardcode the project; make it work with arbitrary websites.
+(defvar tlon-color-palette-file
+  (file-name-concat default-directory "tlon-color-palettes.el")
+  "File where custom color palettes are persisted.")
+
+(defvar tlon-color-globals-css-file
+  (file-name-concat (tlon-repo-lookup :dir :name "uqbar-front")
+		    "src/styles/globals.css")
+  "Path to the `globals.css' file.")
+
+(defvar tlon-color-theme-colors-js-file
+  (file-name-concat (tlon-repo-lookup :dir :name "uqbar-front")
+		    "src/app/theme-colors.js")
+  "Path to the `theme-colors.js' file.")
+
+;;;;; Frontend elements
+
+;;;;;; CSS
+
+(defconst tlon-color-globals-css-header
   "@media (prefers-color-scheme: %s) {\n"
-  "CSS header for each theme section.
+  "Header for each theme section in the `globals.css' file.
 The placeholder %s is replaced with either \"light\" or \"dark\".")
 
-(defconst tlon-color-css-footer
-  "\n    }\n}\n"
-  "CSS footer for each theme section.")
+(defconst tlon-color-globals-css-footer
+  "\n    }\n}\n\n"
+  "Footer for each theme section in the `globals.css' file.")
+
+(defconst tlon-color-globals-css-remove-pattern
+  (concat (format tlon-color-globals-css-header ".*")
+	  "\\(.\\|\n\\)*?"
+	  tlon-color-globals-css-footer)
+  "Pattern to remove the colors section in the `globals.css' file.")
+
+;;;;;; JS
+
+(defconst tlon-color-theme-colors-js-header
+  "export const %s = {\n"
+  "Header for each theme section in the `theme-colors.js' file.")
+
+(defconst tlon-color-theme-colors-js-footer
+  "\n};\n\n"
+  "Footer for each theme section in the `theme-colors.js' file.")
 
 ;;;; Functions
 
@@ -77,118 +108,185 @@ The placeholder %s is replaced with either \"light\" or \"dark\".")
   (list (completing-read "Palette: "
 			 (mapcar #'car tlon-color-palettes) nil t)))
 
-;;;;; Set palette
+;;;###autoload
+(defun tlon-color-open-globals-file ()
+  "Open the global CSS file."
+  (interactive)
+  (find-file tlon-color-globals-css-file))
+
+;;;;; Load palettes
 
 ;;;###autoload
-(defun tlon-color-set-palette (palette)
-  "Set the color PALETTE."
+(defun tlon-color-load-palette (palette &optional language)
+  "Load color PALETTE for file in LANGUAGE.
+If LANGUAGE is nil, load it for both CSS and JS files."
   (interactive (tlon-color-read-palette))
-  (tlon-color-remove-colors-section)
-  (tlon-color-insert-palette palette))
+  (let ((languages (if language (list language) '(css js))))
+    (dolist (language languages)
+      (tlon-color-remove-colors-section language)
+      (tlon-color-insert-colors-section palette language))))
 
-;;;;;; CSS encoding
+;;;;;; Encoding
 
-(defun tlon-color-encode-css (palette)
-  "Generate CSS for PALETTE."
-  (let (css)
-    (dolist (theme '(light dark))
-      (push (tlon-color-encode-css-theme palette theme) css))
-    (mapconcat #'identity (reverse css) "\n")))
+(defun tlon-color-insert-colors-section (palette language)
+  "Insert PALETTE into the LANGUAGE file."
+  (let ((encoded (tlon-color-encode palette language))
+	(file (pcase language
+		('css tlon-color-globals-css-file)
+		('js tlon-color-theme-colors-js-file))))
+    (save-excursion
+      (with-current-buffer (find-file-noselect file)
+	(goto-char (point-min))
+	(insert encoded)
+	;; FIXME: for the time being we always use `hsl' format for the
+	;; `theme-colors.js' file, since the site will otherwise break.
+	(if (string= (buffer-file-name) tlon-color-globals-css-file)
+	    (color-extras-convert-all tlon-color-format-in-frontend-files)
+	  (color-extras-convert-all "hsl"))
+	(save-buffer)))))
 
-(defun tlon-color-encode-css-theme (palette theme)
-  "Generate css for PALETTE THEME.
-THEME is either `light' or `dark'."
-  (string-chop-newline
-   (concat
-    (format tlon-color-css-header theme)
-    "    :root {\n"
-    (tlon-tlon-encode-css-variables palette theme)
-    tlon-color-css-footer)))
+;; TODO: add name of palette as a comment line in the CSS file
+(defun tlon-color-encode (palette language)
+  "Encode PALETTE in LANGUAGE."
+  (let ((fun (pcase language
+	       ('css #'tlon-color-encode-globals-css)
+	       ('js #'tlon-color-encode-theme-colors-js)))
+	(encoded))
+    (dolist (theme '(dark light))
+      (push (funcall fun palette theme) encoded))
+    (mapconcat #'identity encoded)))
 
-(defun tlon-tlon-encode-css-variables (palette theme)
-  "Encode CSS variables for PALETTE THEME."
+(defun tlon-color-encode-globals-css (palette theme)
+  "Generate `globals.css' section for THEME in PALETTE.
+  THEME is either `light' or `dark'."
+  (concat
+   (format tlon-color-globals-css-header theme)
+   "    :root {\n"
+   (tlon-tlon-encode-frontend-variables palette theme 'css)
+   tlon-color-globals-css-footer))
+
+(defun tlon-color-encode-theme-colors-js (palette theme)
+  "Generate `theme-colors.js' section for THEME in PALETTE.
+  THEME is either `light' or `dark'."
+  (concat
+   (format tlon-color-theme-colors-js-header (pcase theme
+					       ('light "LIGHT_COLORS")
+					       ('dark "DARK_COLORS")))
+   (tlon-tlon-encode-frontend-variables palette theme 'js)
+   tlon-color-theme-colors-js-footer))
+
+(defun tlon-tlon-encode-frontend-variables (palette theme language)
+  "Encode frontend variables for PALETTE of THEME.
+  THEME is either `light' or `dark'. LANGUAGE is either `css' or `js'."
   (mapconcat (lambda (line)
 	       (let* ((variable (car line))
 		      (cons (cdr line))
 		      (value (pcase theme
 			       ('light (car cons))
-			       ('dark (cdr cons)))))
-		 (format "        %s: %s;" variable value)))
+			       ('dark (cdr cons))))
+		      (format-string (pcase language
+				       ('css "        %s: %s;")
+  ('js "  \"%s\": \"%s\","))))
+		 (format format-string variable value)))
 	     (alist-get palette tlon-color-palettes nil nil #'string=) "\n"))
 
-(defun tlon-color-remove-colors-section ()
-  "Remove the colors section in the CSS file."
-  (let ((pattern (concat (format tlon-color-css-header ".*")
-			 "\\(.\\|\n\\)*?"
-			 tlon-color-css-footer)))
-    (save-excursion
-      (with-current-buffer (find-file-noselect tlon-color-globals-file)
-	(goto-char (point-min))
-	(while (re-search-forward pattern nil t)
-	  (replace-match ""))
+(defun tlon-color-remove-colors-section (language)
+  "Remove the colors section in the LANGUAGE file."
+  (save-excursion
+    (with-current-buffer (find-file-noselect (pcase language
+					       ('css tlon-color-globals-css-file)
+					       ('js tlon-color-theme-colors-js-file)))
+      (pcase language
+	('css
+	 (goto-char (point-min))
+	 (while (re-search-forward tlon-color-globals-css-remove-pattern nil t)
+	   (replace-match "")))
+	('js
+	 (erase-buffer))
 	(save-buffer)))))
 
-(defun tlon-color-insert-palette (palette)
-  "Insert PALETTE into the CSS file."
-  (let ((css (tlon-color-encode-css palette)))
-    (save-excursion
-      (with-current-buffer (find-file-noselect tlon-color-globals-file)
-	(goto-char (point-min))
-	(insert css)
-	(save-buffer)))))
+(defun tlon-color-save-frontend-files ()
+  "Save `globals.css' and propagate its state to `theme-colors.js'."
+  (save-excursion
+    (with-current-buffer (find-file-noselect tlon-color-globals-css-file))
+    (save-buffer))
+  (tlon-color-store-palette "temp-palette" 'overwrite)
+  (tlon-color-load-palette "temp-palette" 'js))
 
-;;;;; Add & remove palettes
-
-;;;###autoload
-(defun tlon-color-add-palette (name)
-  "Add parsed palette with NAME to `tlon-color-palettes'."
-  (interactive "MPalette Name: ")
-  (let ((new-palette (cons name (tlon-color-parse-css))))
-    (unless (assoc name tlon-color-palettes)
-      (push new-palette tlon-color-palettes)
-      (tlon-color-save-palettes)
-      (message "Palette %s added and saved successfully." name))))
+;;;;; Store & delete palettes
 
 ;;;###autoload
-(defun tlon-color-remove-palette (name)
-  "Remove the palette with the given NAME from `tlon-color-palettes'."
+(defun tlon-color-store-palette (palette-name &optional overwrite)
+  "Store the parsed PALETTE-NAME in `tlon-color-palettes'.
+If PALETTE-NAME already exists, asks for confirmation to overwrite it, unless
+OVERWRITE is non-nil."
+  (interactive "MPalette name: ")
+  (let* ((parsed-palette (cons palette-name (tlon-color-parse-css)))
+         (existing-palette (assoc palette-name tlon-color-palettes))
+	 (format-string "Palette %s %s.")
+	 action)
+    (if existing-palette
+        (when (or overwrite (yes-or-no-p "Palette already exists. Overwrite? "))
+          (setcdr existing-palette (cdr parsed-palette))
+	  (setq action "updated"))
+      (push parsed-palette tlon-color-palettes)
+      (setq action "added"))
+    (tlon-color-save-palettes-to-file)
+    (message format-string palette-name action)))
+
+;;;###autoload
+(defun tlon-color-delete-palette (palette)
+  "Delete stored color PALETTE from `tlon-color-palettes'."
   (interactive (tlon-color-read-palette))
   (setq tlon-color-palettes
-        (assoc-delete-all name tlon-color-palettes nil))
-  (tlon-color-save-palettes)  ;; Call save function if you want changes to persist.
-  (message "Palette `%s` removed successfully." name))
+	(assoc-delete-all palette tlon-color-palettes nil))
+  (tlon-color-save-palettes-to-file)  ;; Call save function if you want changes to persist.
+  (message "Palette `%s` removed successfully." palette))
 
 ;;;;;; CSS parsing
 
 (defun tlon-color-parse-css ()
   "Parse the CSS file and extract color variables and their values for each theme."
-  (let ((css-buffer (find-file-noselect tlon-color-globals-file)))
-    (let ((light-collect (tlon-color-parse-css-theme css-buffer 'light))
-          (dark-collect (tlon-color-parse-css-theme css-buffer 'dark)))
-      (kill-buffer css-buffer)
-      (mapcar (lambda (light-var)
-                (cons (car light-var)
-                      (cons (cdr light-var)
-                            (cdr (assoc (car light-var) dark-collect)))))
-              light-collect))))
+  (let* ((css-buffer (find-file-noselect tlon-color-globals-css-file))
+	 (light-collect (tlon-color-parse-css-theme css-buffer 'light))
+	 (dark-collect (tlon-color-parse-css-theme css-buffer 'dark)))
+    (mapcar (lambda (light-var)
+	      (cons (car light-var)
+		    (cons (cdr light-var)
+			  (cdr (assoc (car light-var) dark-collect)))))
+	    light-collect)))
 
+(declare-function color-extras-convert-all "color-extras")
 (defun tlon-color-parse-css-theme (css-buffer theme)
-  "Parse the CSS-BUFFER for a THEME and return its color definitions as an alist."
-  (let (theme-collect)
+  "Parse the CSS-BUFFER for a THEME and return its color definitions as an alist.
+To ensure that colors are stored in a consistent format, they are returned in
+hex format irrespective of their original format."
+  (let ((temp-file (make-temp-file "tlon-color-parse-css-theme"))
+	(theme-collect)
+	(css-buffer-contents))
     (with-current-buffer css-buffer
-      (goto-char (point-min))
-      (re-search-forward (format tlon-color-css-header theme))
-      (while (re-search-forward "--\\(.*?\\): \\(.*?\\);"
-				(save-excursion
-				  (re-search-forward tlon-color-css-footer) (point)) t)
-        (push (cons (intern (concat "--" (match-string-no-properties 1)))
-                    (match-string-no-properties 2))
-              theme-collect)))
-    theme-collect))
+      (setq css-buffer-contents (buffer-string)))
+    (let ((temp-buf (find-file-noselect temp-file)))
+      (unwind-protect
+	  (with-current-buffer temp-buf
+	    (insert css-buffer-contents)
+	    (color-extras-convert-all "hex" temp-file)
+	    (goto-char (point-min))
+	    (re-search-forward (format tlon-color-globals-css-header theme))
+	    (while (re-search-forward "--\\(.*?\\): \\(.*?\\);"
+				      (save-excursion
+					(re-search-forward tlon-color-css-footer) (point)) t)
+	      (push (cons (intern (concat "--" (match-string-no-properties 1)))
+			  (match-string-no-properties 2))
+		    theme-collect))
+	    (save-buffer)
+	    (kill-buffer temp-buf))
+	(delete-file temp-file))
+      (reverse theme-collect))))
 
 ;;;;; Sessions
 
-(defun tlon-color-save-palettes ()
+(defun tlon-color-save-palettes-to-file ()
   "Save the current color palettes to the file."
   (with-temp-file tlon-color-palette-file
     (insert ";;; tlon Color Palettes -*- lexical-binding: t -*-\n\n")
@@ -281,13 +379,19 @@ COMPONENT can be either \"h\", \"s\" or \"l\". DIRECTION can be either `+' or
 
 ;;;;; Menu
 
+(transient-define-infix tlon-color-format-in-frontend-files-infix ()
+  :class 'transient-lisp-variable
+  :variable 'tlon-color-format-in-frontend-files
+  :reader (lambda (_ _ _) (completing-read ": " '("hex" "hsl"))))
+
+(transient-define-infix tlon-color-change-step-infix ()
+  :class 'transient-lisp-variable
+  :variable 'tlon-colors-change-step
+  :reader (lambda (_ _ _)  (string-to-number (read-string "Step: "))))
+
 ;;;###autoload (autoload 'tlon-color-menu "tlon-color" nil t)
 (transient-define-prefix tlon-color-menu ()
   "`tlon-color' menu."
-  ["Palette"
-   ("a" "Add"               tlon-color-add-palette)
-   ("r" "Remove"            tlon-color-remove-palette)
-   ("s" "Set"               tlon-color-set-palette)])
   [["Change colors"
     ""
     "hue (H)"
@@ -307,6 +411,18 @@ COMPONENT can be either \"h\", \"s\" or \"l\". DIRECTION can be either `+' or
     ""
     "Options"
     ("-s" "Change step"                    tlon-color-change-step-infix)]
+   ["Palette"
+    ""
+    ("o" "Load"                            tlon-color-load-palette)
+    ("t" "Store"                           tlon-color-store-palette)
+    ("d" "Delete"                          tlon-color-delete-palette)
+    ""
+    ("g" "Open ‘globals.css’ file"         tlon-color-open-globals-file)
+    ("c" "Convert colors"                  color-extras-convert-all)
+    ("C" "Calculate contrast"              color-extras-contrast)
+    ""
+    "Options"
+    ("-f" "Format in frontend files"       tlon-color-format-in-frontend-files-infix)]])
 
 (provide 'tlon-color)
 ;;; tlon-color.el ends here
