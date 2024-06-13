531a3bd036faba869b4b4f5f91c6018ab1726818
Pablo Stafforini <pablo@stafforini.com>
2024-06-13 17:03:05 -0300

    tts: position notes in separate paragrapphs

--- tlon-tts.el
+++ tlon-tts.el
@@ -1296,7 +1296,8 @@ citation key, format. Hence, it must be run *before*
 (defun tlon-tts-handle-note (note)
   "Handle NOTE for audio narration."
   (let ((clean-note (replace-regexp-in-string (tlon-md-get-tag-pattern "Sidenote") "" note)))
-    (tlon-tts-listener-cue-full-enclose tlon-tts-note-cues clean-note)))
+    (format "\n\n%s\n"
+	    (tlon-tts-listener-cue-full-enclose tlon-tts-note-cues clean-note))))
 
 ;;;;;;;; Formatting
 
