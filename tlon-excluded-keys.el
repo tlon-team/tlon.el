cae543e403f81c685c39314c5e01440c0511a866
Pablo Stafforini <pablo@stafforini.com>
2024-06-21 13:49:03 -0300

    Add tlon-excluded-keys.el

---
+++ tlon-excluded-keys.el
@@ -0,0 +1,7 @@
+;;; Tlon excluded keys -*- lexical-binding: t -*-
+
+;; This file contains the keys to be excluded from `tlon-tex-entries-report'.
+
+(setq tlon-tex-no-abstract-needed '("Marshall1998DictionarySociology" "CenterforHuman-CompatibleArtificialIntelligence2021" "CenterforHuman" "CenterforEmergingRiskResearch2021Transparency" "Bostrom2021HomePage" "BecksteadFundamentalsResearchMacrostrategy" "Aird2020CollectionOfSources" "1989OhioV"))
+
+(provide 'tlon-tex-no-abstract-needed)
