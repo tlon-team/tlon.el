6af8c38ddc501bfb9a35480ac8ebaf4f89d560a2
Pablo Stafforini <pablo@stafforini.com>
2024-06-22 08:11:21 -0300

    excluded-keys: update list of excluded keys

--- tlon-excluded-keys.el
+++ tlon-excluded-keys.el
@@ -1,7 +1,7 @@
-;;; Tlon excluded keys -*- lexical-binding: t -*-
+;;; tlon-excluded-keys.el -*- lexical-binding: t -*-
 
 ;; This file contains the keys to be excluded from `tlon-tex-entries-report'.
 
-(setq tlon-tex-no-abstract-needed '("Marshall1998DictionarySociology" "CenterforHuman-CompatibleArtificialIntelligence2021" "CenterforHuman" "CenterforEmergingRiskResearch2021Transparency" "Bostrom2021HomePage" "BecksteadFundamentalsResearchMacrostrategy" "Aird2020CollectionOfSources" "1989OhioV"))
+(setq tlon-tex-excluded-keys '("Thomas1974PhilosophicRadicals" "Spitz1962FreedomIndividualityMill" "Marshall1998DictionarySociology" "CenterforHuman-CompatibleArtificialIntelligence2021" "CenterforHuman" "CenterforEmergingRiskResearch2021Transparency" "Bostrom2021HomePage" "BecksteadFundamentalsResearchMacrostrategy" "Aird2020CollectionOfSources" "1989OhioV" "Metamed2013AboutOurScientists" "Macaskill2016FiresideChatWith" "Greene2001PsychologicalPerspectiveNozick" "CenterforEmergingRiskResearch2021Transparency" "FutureofHumanityInstitute2022CarrickFlynn" "Crunchbase2023EatJust"))
 
-(provide 'tlon-tex-no-abstract-needed)
+(provide 'tlon-excluded-keys.el)
