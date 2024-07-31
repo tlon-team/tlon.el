f35d188d56533667d525ea75db94e3a112f87dd3
Pablo Stafforini <pablo@stafforini.com>
2024-07-31 18:47:15 -0300

    Update to tlon version 1.5.7

--- tlon.el
+++ tlon.el
@@ -2,7 +2,7 @@
 
 ;; Author: Pablo Stafforini
 ;; Maintainer: Pablo Stafforini
-;; Version: 1.5.6
+;; Version: 1.5.7
 ;; URL: https://github.com/tlon-team/tlon
 ;; Keywords: convenience tools
 
@@ -42,7 +42,7 @@
 
 ;;;; Variables
 
-(defconst tlon-version "1.5.6"
+(defconst tlon-version "1.5.7"
   "Version of the `tlon' package.")
 
 ;;;;; Files and dirs
