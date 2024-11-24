e2d15f1184ef3fa332437b2d84310d4fb5badbe7
Pablo Stafforini <pablo@stafforini.com>
2024-11-24 19:11:24 -0300

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((emacs-lisp-mode . (;; Aspell does not currently support Korean & Arabic, so we just disable it
+		     (eval . (jinx-mode -1)))))
