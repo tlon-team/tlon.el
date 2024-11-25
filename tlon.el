e4164b497dd5c6144e6b85e1e6e39a41f141398b
Pablo Stafforini <pablo@stafforini.com>
2024-11-25 11:33:08 -0300

    Update to tlon version 1.6.4

--- tlon.el
+++ tlon.el
@@ -2,7 +2,7 @@
 
 ;; Author: Pablo Stafforini
 ;; Maintainer: Pablo Stafforini
-;; Version: 1.6.3
+;; Version: 1.6.4
 ;; URL: https://github.com/tlon-team/tlon
 ;; Keywords: convenience tools
 ;; Package-Requires: ((bibtex-extras "0.1")
@@ -52,7 +52,7 @@
 
 ;;;; Variables
 
-(defconst tlon-version "1.6.3"
+(defconst tlon-version "1.6.4"
   "Version of the `tlon' package.")
 
 ;;;;; Files and dirs
