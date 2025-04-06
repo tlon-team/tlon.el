8e1d3a8d19e1b69db6bd5ea918cbf7a563dff22d
Pablo Stafforini <pablo@stafforini.com>
2025-04-06 09:15:30 -0300

    fix duplicate parens

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 ((emacs-lisp-mode . (;; Aspell does not currently support Korean & Arabic, so we just disable it
 		     (eval . (jinx-mode -1))
-		     ((fill-column . 80)))))
+		     (fill-column . 80))))
