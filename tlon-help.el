90d17e39578c21dcf4ed72ca9874c62fe99a08c6
Pablo Stafforini <pablo@stafforini.com>
2025-04-17 15:43:53 -0300

    ai: reorder section

--- tlon-help.el
+++
@@ -1,6 +0,0 @@
-;;; tlon-help.el --- Help functionality for Tlön -*- lexical-binding: t -*-
-
-;; This file is now empty as its contents have been moved to tlon-ai.el.
-;; It can be safely deleted.
-
-;;; tlon-help.el ends here
