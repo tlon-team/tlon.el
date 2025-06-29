63dc2682764d8e1c768d5a23aeefdcbb4683d5a2
Pablo Stafforini <pablo@stafforini.com>
2025-06-29 09:32:31 -0300

    dispatch: use consistent key bindings

--- tlon-dispatch.el
+++ tlon-dispatch.el
@@ -65,7 +65,7 @@
     ("u" "counterpart"                    tlon-counterpart-menu)
     ("v" "url"                            tlon-url-menu)
     ("y" "forg"                           tlon-forg-menu)
-    ("Y" "youtube"                        tlon-youtube-menu)
+    ("H-y" "youtube"                      tlon-youtube-menu)
     ("x" "tex"                            tlon-tex-menu)
     ("z" "tts"                            tlon-tts-menu)]
    [""
