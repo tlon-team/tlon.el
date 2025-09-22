4fa10f9a8d29a5811b405644736559612574d215
Pablo Stafforini <pablo@stafforini.com>
2025-09-22 19:18:25 -0300

    Delete accidentally pushed file

--- tlon-excluded-keys.el
+++
@@ -1,8 +0,0 @@
-;;; Tlon excluded keys -*- lexical-binding: t -*-
-
-;; This file contains the keys to be excluded from `tlon-bib-entries-report'.
-
-(defvar tlon-bib-excluded-keys)
-(setq tlon-bib-excluded-keys '("TomasikAnimalsExperienceNet" "Spitz1962FreedomAndIndividuality" "Spitz1962FreedomAndIndividuality" "Anonymous1673GrandConcernEngland" "North1744LifeHonourableSir" "Phillips2015RationalFaithStudy" "Hutchinson2014EthicsOfExtending" "Thomas1974PhilosophicRadicals" "Spitz1962FreedomIndividualityMill" "Marshall1998DictionarySociology" "CenterforHuman-CompatibleArtificialIntelligence2021" "CenterforHuman" "CenterforEmergingRiskResearch2021Transparency" "Bostrom2021HomePage" "BecksteadFundamentalsResearchMacrostrategy" "Aird2020CollectionOfSources" "1989OhioV" "Metamed2013AboutOurScientists" "Macaskill2016FiresideChatWith" "Greene2001PsychologicalPerspectiveNozick" "CenterforEmergingRiskResearch2021Transparency" "FutureofHumanityInstitute2022CarrickFlynn" "Crunchbase2023EatJust"))
-
-(provide 'tlon-bib-excluded-keys)
