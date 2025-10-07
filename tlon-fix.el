10dec369ad633100ca9d019f7d27bc68f0495b6f
Pablo Stafforini <pablo@stafforini.com>
2025-10-07 09:24:45 -0300

    fix: remove commented out line

--- tlon-fix.el
+++ tlon-fix.el
@@ -229,7 +229,6 @@ Do not perform these replacements if the terms occur in math formulae, links, or
 match certain words that should not be altered, such as \"80,000 Hours\"."
   (interactive)
   (let* ((exclusions `(,(tlon-md-get-tag-pattern "Math")
-		       ;; ,markdown-regex-link-inline ; why is this included?
 		       ,ffap-url-regexp
 		       "80,000 Hours"))
 	 (exclusion-patterns (mapconcat #'identity exclusions "\\|"))
