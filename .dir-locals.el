c014d866865133d56db2c8a4abb03e33aa7a1c51
Pablo Stafforini <pablo@stafforini.com>
2026-03-05 12:44:49 -0300

    Add .dir-locals.el org-mode hook for auto Texinfo export on save

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,10 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 ((emacs-lisp-mode . (;; Aspell does not currently support Korean & Arabic, so we just disable it
 		     (eval . (jinx-mode -1))
-		     (fill-column . 80))))
+		     (fill-column . 80)))
+ (org-mode . ((eval . (add-hook 'after-save-hook
+                                (lambda ()
+                                  (require 'ox-texinfo)
+                                  (let ((inhibit-message t))
+                                    (org-texinfo-export-to-texinfo)))
+                                nil t)))))
